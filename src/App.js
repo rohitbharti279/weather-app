c06daed9f461bda948d929db3e5ae0354acebb20
Rohit Bharti <rohitbharti279@gmail.com>
2023-10-09 17:23:13 +0530

    Css part

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className='top flex flex-col justify-center items-center'>
         <h1 className='text-5xl font-bold flex'>🌞<span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-violet-500 md:tracking-wider text-center"> Weather App</span>⛈️</h1>
         <input type='text' className='p-1.5 px-4 mt-5 rounded'></input>
-        <button className='mt-3 p-1 rounded-md text-xl text-white font-medium bg-slate-500'>Search</button>
+        <button className='mt-3 p-1 px-3 rounded-md text-xl text-white font-medium bg-slate-500'>Search</button>
       </div>
       <div className='bottom flex flex-col  items-center'>
         <div className='shadow-2xl rounded-md p-5 px-10 md:h-[45vh] md:w-[40%] flex flex-col justify-center items-center'>
